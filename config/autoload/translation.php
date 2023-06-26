6565dd42083a6d4494340ecd95a7af69d49645cb
AuroraYolo <847050412@qq.com>
2023-06-26 22:22:26 +0800

    :sparkles: Add UserRequest
    .

---
+++ config/autoload/translation.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Cloud-Admin.
+ *
+ * @link     https://www.cloud-admin.jayjay.cn
+ * @document https://wiki.cloud-admin.jayjay.cn
+ * @license  https://github.com/swow-cloud/swow-admin/blob/master/LICENSE
+ */
+return [
+    'locale' => 'zh_CN',
+    'fallback_locale' => 'en',
+    'path' => BASE_PATH . '/storage/languages',
+];

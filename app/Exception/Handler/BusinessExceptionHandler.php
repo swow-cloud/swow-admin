55c7da8d78b0166df96c95bf4d109005088081ab
AuroraYolo <847050412@qq.com>
2023-07-25 00:07:14 +0800

    Remove redundant message from server error

--- app/Exception/Handler/BusinessExceptionHandler.php
+++ app/Exception/Handler/BusinessExceptionHandler.php
@@ -51,7 +51,7 @@ class BusinessExceptionHandler extends ExceptionHandler
 
         $this->logger->error(formatThrowable($throwable));
 
-        return $this->response->fail(ErrorCode::SERVER_ERROR->value, 'Server Error');
+        return $this->response->fail(ErrorCode::SERVER_ERROR->value);
     }
 
     public function isValid(Throwable $throwable): bool

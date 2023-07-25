d52dbace89b45b857c0329d3c30bfbf43d502e14
AuroraYolo <847050412@qq.com>
2023-07-25 21:36:48 +0800

    Add TcpDebugger and WsDebugger classes
    This commit introduces two classes, TcpDebugger and WsDebugger, under CloudAdmin\SDB namespace. These classes extend from the base Debugger. The addition of these classes is exclusive of any functional modifications and is directed towards enhancing the interactivity feature of SDB through web pages including websocket and TCP connections which is indicated in the todo list update.

---
+++ cloud-admin/SDB/TcpDebugger.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Cloud-Admin.
+ *
+ * @link     https://www.cloud-admin.jayjay.cn
+ * @document https://wiki.cloud-admin.jayjay.cn
+ * @license  https://github.com/swow-cloud/swow-admin/blob/master/LICENSE
+ */
+namespace CloudAdmin\SDB;
+
+use Swow\Debug\Debugger\Debugger;
+
+class TcpDebugger extends Debugger
+{
+}

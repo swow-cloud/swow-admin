5b5b3bade1d3e818a903174723387451d6d8fbb6
AuroraYolo <847050412@qq.com>
2023-09-11 14:38:35 +0800

    去除无用的代码

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -11,26 +11,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use function CloudAdmin\Utils\logger;
-use function CloudAdmin\Utils\stdout;
-
 class IndexController extends AbstractController
 {
     public function index()
     {
         $user = $this->request->input('user', 'Hyperf');
         $method = $this->request->getMethod();
-        logger()->error('test error');
-        logger()->info('test info');
-        logger()->debug('test debug');
-        logger()->alert('test alert');
-        stdout()->info('1111222');
-        // 测试并发写入
-        \Hyperf\Engine\Coroutine::create(function () {
-            for ($i = 0; $i < 100000; ++$i) {
-                //                stdout()->info($i . 'abc');
-            }
-        });
 
         return [
             'method' => $method,

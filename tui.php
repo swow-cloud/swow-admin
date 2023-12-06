c27df0f170483242239f8e57ce16c1eaa9228c36
AuroraYolo <847050412@qq.com>
2023-12-06 23:23:41 +0800

    Refactor tui.php and update server path in annotations
    In tui.php, the interface is streamlined for simplicity and improved readability. Unused import statements and methods like `BlockWidget`, `Direction`, `CanvasWidget` and `GridWidget` are removed, while new ones like `TextShape`, `AnsiColor`, `Marker` and `FloatPosition` are added. The diff also shows changes in the 'config/autoload/annotations.php' where the path to the 'Server' class is corrected.

--- tui.php
+++ tui.php
@@ -1,115 +1,52 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of Cloud-Admin project.
- *
- * @link     https://www.cloud-admin.jayjay.cn
- * @document https://wiki.cloud-admin.jayjay.cn
- * @license  https://github.com/swow-cloud/swow-admin/blob/master/LICENSE
- */
+
 use PhpTui\Tui\DisplayBuilder;
-use PhpTui\Tui\Extension\Core\Shape\MapResolution;
-use PhpTui\Tui\Extension\Core\Shape\MapShape;
-use PhpTui\Tui\Extension\Core\Widget\Block\Padding;
+use PhpTui\Tui\Extension\Bdf\Shape\TextShape;
 use PhpTui\Tui\Extension\Core\Widget\BlockWidget;
 use PhpTui\Tui\Extension\Core\Widget\CanvasWidget;
 use PhpTui\Tui\Extension\Core\Widget\GridWidget;
 use PhpTui\Tui\Extension\Core\Widget\ParagraphWidget;
+use PhpTui\Tui\Model\Color\AnsiColor;
 use PhpTui\Tui\Model\Direction;
 use PhpTui\Tui\Model\Layout\Constraint;
+use PhpTui\Tui\Model\Marker;
+use PhpTui\Tui\Model\Position\FloatPosition;
 use PhpTui\Tui\Model\Text\Text;
 use PhpTui\Tui\Model\Text\Title;
 use PhpTui\Tui\Model\Widget\Borders;
-use PhpTui\Tui\Model\Widget\BorderType;
 
 require 'vendor/autoload.php';
 
 $display = DisplayBuilder::default()->build();
-$display->clear();
 $display->draw(
     GridWidget::default()
         ->direction(Direction::Horizontal)
         ->constraints(
             Constraint::percentage(50),
-            Constraint::percentage(50)
+            Constraint::percentage(50),
         )
         ->widgets(
-            BlockWidget::default()
-                ->titles(Title::fromString('Left'))
-                ->padding(Padding::all(2))
-                ->borders(Borders::ALL)
-                ->borderType(BorderType::Rounded)
-                ->widget(
-                    CanvasWidget::fromIntBounds(-180, 180, -90, 90)
-                        ->draw(
-                            MapShape::default()->resolution(MapResolution::High)
-                        ),
-                ),
-            BlockWidget::default()
-                ->titles(Title::fromString('Right'))
-                ->padding(Padding::all(2))
-                ->borders(Borders::ALL)
-                ->borderType(BorderType::Rounded)
-                ->widget(
-                    ParagraphWidget::fromText(
-                        Text::parse(
-                            <<<'EOT'
+            BlockWidget::default()->borders(Borders::ALL)->titles(Title::fromString('Left')),
+            GridWidget::default()
+                ->direction(Direction::Vertical)
+                ->constraints(
+                    Constraint::percentage(50),
+                    Constraint::percentage(50),
+                )
+                ->widgets(
+                    BlockWidget::default()->borders(Borders::ALL)->titles(Title::fromString('Top Right'))->widget(
+                        ParagraphWidget::fromText(
+                            Text::parse(<<<'EOT'
                             The <fg=green>world</> is the totality of <options=bold>entities</>,
                             the whole of reality, or everything that is.[1] The nature of the
                             world has been <fg=red>conceptualized</> differently in different fields. Some
                             conceptions see the world as unique while others talk of a
                             plurality of <bg=green>worlds</>.
-                            EOT
-                        )
-                    )
-                ),
+                            EOT)
+                        )),
+                    BlockWidget::default()->borders(Borders::ALL)->titles(Title::fromString('Bottom Right')),
+                )
         )
 );
-$i = 0;
-while (true) {
-    ++$i;
-    if ($i > 100) {
-        break;
-    }
-    $display->draw(
-        GridWidget::default()
-            ->direction(Direction::Horizontal)
-            ->constraints(
-                Constraint::percentage(50),
-                Constraint::percentage(50)
-            )
-            ->widgets(
-                BlockWidget::default()
-                    ->titles(Title::fromString('Left'))
-                    ->padding(Padding::all(2))
-                    ->borders(Borders::ALL)
-                    ->borderType(BorderType::Rounded)
-                    ->widget(
-                        CanvasWidget::fromIntBounds(-180, 180, -90, 90)
-                            ->draw(
-                                MapShape::default()->resolution(MapResolution::High)
-                            ),
-                    ),
-                BlockWidget::default()
-                    ->titles(Title::fromString('Right'))
-                    ->padding(Padding::all(2))
-                    ->borders(Borders::ALL)
-                    ->borderType(BorderType::Rounded)
-                    ->widget(
-                        ParagraphWidget::fromText(
-                            Text::parse(
-                                <<<'EOT'
-                            The <fg=green>world</> is the totality of <options=bold>entities</>,
-                            the whole of reality, or everything that is.[1] The nature of the
-                            world has been <fg=red>conceptualized</> differently in different fields. Some
-                            conceptions see the world as unique while others talk of a
-                            plurality of <bg=green>worlds</>.
-                            EOT
-                            )
-                        )
-                    ),
-            )
-    );
-    \sleep(3);
-}
